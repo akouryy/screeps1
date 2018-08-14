6d2902a1543b47a2add412557ce47782d8f9ec59
akouryy <akouryy7@gmail.com>
2018-08-14 21:43:16 +0900

    Remove `sourcesContent` from output source map

--- tools/rollup/source-map-replace.js
+++ tools/rollup/source-map-replace.js
@@ -2,6 +2,31 @@
 // import { Plugin, OutputOptions, SourceDescription } from 'rollup';
 import * as fs from 'fs';
 
+const dblStrRgxPart = `
+  "
+    (?: [^ " \\n \\\\]+ | \\\\ (?:
+      [^0-9xu \\n]            # SingleEscapeCharacter or NonEscapeCharacter
+    | 0 (?! \\d)
+    | x [0-9a-fA-F]{2}        # HexEscapeSequence
+    | u [0-9a-fA-F]{4}        # UnicodeEscapeSequence
+    | u \\{ [0-9a-fA-F]+ \\}
+    | \\n                     # LineContinuation
+    ))*
+  "
+`.replace(/#.+/g, '').replace(/\s+/g, '');
+
+const sourcesContentRgxPart = `
+  "sourcesContent"
+  \\s*:\\s*
+  \\[
+    ${dblStrRgxPart}
+    (?:,${dblStrRgxPart})*
+  \\]
+  ,
+`.replace(/#.+/g, '').replace(/\s+/g, '');
+
+const sourcesContentRgx = new RegExp(sourcesContentRgxPart, 'g');
+
 export default function sourceMapReplace() {
   const plugin/*: Plugin*/ = {
     name: "myReplace",
@@ -11,6 +36,7 @@ export default function sourceMapReplace() {
         const fname = options.file + '.map';
         let data = fs.readFileSync(fname).toString();
         data = data.replace('\u2028', '\\u2028').replace('\u2029', '\\u2029');
+        data = data.replace(sourcesContentRgx, ''),
         fs.writeFileSync(fname, data);
       }
     },
